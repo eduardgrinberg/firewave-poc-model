a77ffc3f08007a31823b3b08138940d68abf1479
eduardgrinberg <eduardgrinberg@gmail.com>
2023-05-12 19:29:55 +0300

    renamed models

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from training import Training
 
 model = Training.create_and_train_model(3)
 
-torch.save(model.state_dict(), Path.cwd() / 'models' / 'model.pt')
+torch.save(model.state_dict(), Path.cwd() / 'models' / 'model_tmp.pt')
 
 ds = SoundDS('sound\\test')
 dl = DataLoader(ds, batch_size=1, shuffle=False)
-model = Model.load_from_file(Path.cwd() / 'models' / 'model.pt')
+model = Model.load_from_file(Path.cwd() / 'models' / 'model_tmp.pt')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # model = model.to(device)
--- test.py
+++ test.py
@@ -11,13 +11,13 @@ from model import Model
 from soundDs import SoundDS
 
 model = Model()
-model.load_state_dict(torch.load(Path('models') / 'model.pt'))
+model.load_state_dict(torch.load(Path('models') / 'model_esp32_youtube.pt'))
 model.eval()
 
 
-ds = SoundDS('sound\\test')
+ds = SoundDS('..\\sound\\test')
 dl = DataLoader(ds, batch_size=1, shuffle=False)
-model = Model.load_from_file(Path.cwd() / 'models' / 'model.pt')
+model = Model.load_from_file(Path.cwd() / 'models' / 'model_esp32_youtube.pt')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # model = model.to(device)
